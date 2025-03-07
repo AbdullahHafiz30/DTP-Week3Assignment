5f703d731f46cd5967fc5bb2b28cd639f18e8002
ABDULLAH HAFIZ <ABDULLAH_MH30@OUTLOOK.COM>
2025-03-07 22:00:20 +0300

    Initial Commit

---
+++ Expenses/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Expenses
+//
+//  Created by عبدالله حافظ on 07/09/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Expenses/ExpensesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpensesApp.swift
+//  Expenses
+//
+//  Created by عبدالله حافظ on 07/09/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct ExpensesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
